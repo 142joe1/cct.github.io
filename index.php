0a8543e5502989845faffcd69a20b3209ef507d0
142joe1 <programmingproject06@gmail.com>
2024-10-26 10:49:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -184,7 +184,7 @@
 <body>
     <div class="container">
         <h1>Welcome to YouChews!!</h1>
-        <p>Please choose an option below to continue:</p>
+        <p>Please choose an option below to continue:!</p>
         <ul>
             <li><a href="login.php">Login</a></li>
             <li><a href="signup.php">Signup</a></li>
